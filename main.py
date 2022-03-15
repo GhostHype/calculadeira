628daa474f8fef9a0713bbcf1b593e37a93f3b72
GhostHype <78053137+GhostHype@users.noreply.github.com>
2022-03-14 21:53:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print('calculadeira by ghostozoraipe\n')
+
+x = int(input('Primeiro Número\n>>>'))
+y = int(input('Segundo Número\n>>>'))
+
+print(f'Soma: {x + y}')
+print(f'Subtração: {x - y}')
+print(f'Divisão: {x / y}')
+print(f'Multiplicação: {x * y}')
+ 
